8e67b8226222c3297d64a086437723e592dc6ffe
red_comrade <87092187+Maaakson@users.noreply.github.com>
2021-12-04 03:30:49 +0400

    App template

---
+++ Swiftfull Thinking Beginner/Swiftfull Thinking Beginner/Swiftfull_Thinking_BeginnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftfull_Thinking_BeginnerApp.swift
+//  Swiftfull Thinking Beginner
+//
+//  Created by Максим Митрофанов on 04.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct Swiftfull_Thinking_BeginnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
